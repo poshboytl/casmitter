6ddb0f378939b9dc77efe6c98aa87e624e9682d4
Terry Tai <poshboytl@gmail.com>
2025-02-21 04:31:27 -0800

    Handmade authenticaiton solution by DHH.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_21_114412) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_21_122523) do
   create_table "attendances", force: :cascade do |t|
     t.integer "attendee_id"
     t.integer "episode_id"
@@ -44,4 +44,23 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_21_114412) do
     t.integer "duration"
     t.string "cover_url"
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
 end
